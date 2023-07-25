fe76602bc9b53df20d2f3529de3efea94c3fa000
tranvantruongtanaakk <129922371+tranvantruongtanaakk@users.noreply.github.com>
2023-07-25 22:53:03 +0700

    init

---
+++ foodpanda_sellers_app/lib/widgets/items_design.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:foodpanda_sellers_app/mainScreens/item_detail_screen.dart';
+import 'package:foodpanda_sellers_app/model/items.dart';
+
+// ignore: must_be_immutable
+class ItemsDesignWidget extends StatefulWidget {
+  Items? model;
+  BuildContext? context;
+
+  ItemsDesignWidget({Key? key, this.model, this.context}) : super(key: key);
+
+  @override
+  _ItemsDesignWidgetState createState() => _ItemsDesignWidgetState();
+}
+
+class _ItemsDesignWidgetState extends State<ItemsDesignWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (c) => ItemDetailsScreen(model: widget.model)));
+      },
+      splashColor: Colors.amber,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: SizedBox(
+          height: 280,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Divider(
+                height: 4,
+                thickness: 3,
+                color: Colors.grey[300],
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Text(
+                widget.model!.title!,
+                style: const TextStyle(
+                  color: Colors.cyan,
+                  fontSize: 18,
+                  fontFamily: "Train",
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Image.network(
+                widget.model!.thumbnailUrl!,
+                height: 220.0,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 2.0,
+              ),
+              Text(
+                widget.model!.shortInfo!,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Divider(
+                height: 4,
+                thickness: 3,
+                color: Colors.grey[300],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
